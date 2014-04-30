a84a8da3c7f4eaf7bd77f44eff2e527283e09329
Sagar Jauhari <sagarjauhari@gmail.com>
2014-04-30 18:28:32 -0400

    Adding files

---
+++ javascripts/elem_automata.js
@@ -0,0 +1,43 @@
+function elem_cell_automata(rule){
+   reset_grid(); 
+  var init_row = [1];
+  for(var row = 0; row < 50; row++){
+    color_grid(init_row, row, 100);
+    init_row = evolve(rule, init_row);
+  }
+}
+
+function out(text){
+    $('#container').append(text+"\n");
+}
+
+function evolve(rule, state){
+    next_state=[];
+    for(var i=-1; i <= state.length; i++){
+        bit = state[i+1] | (state[i] << 1) | (state[i-1] << 2);
+        mask = 1 << bit;
+        next_state[i+1] = ((mask & rule) != 0) ? 1 : 0;
+    }
+    return next_state;
+}
+
+function init_grid(num){
+    for(i=num;i>0;i--){
+        $(".grid").append("<square></square>")
+    }
+}
+
+function reset_grid(num){
+  $(".grid square").css( "background", "#fff" );
+  $(".grid square:nth-child(3n)").css( "background", "#eee" );
+  $(".grid square:nth-child(3n-1)").css( "background", "#ddd" );
+}
+
+function color_grid(vals, row, width){
+  for(var col = 0; col < vals.length; col++){
+    var idx = width/2 - row + (row * width + col);
+    if(vals[col] == 1){
+      $(".grid square:eq(" + idx + ")").css( "background", "#000" );
+    }
+  }
+}
